7826ab3f66b72562f183f8f194dfc8fea81019be
Jorge D. Ortiz-Fuentes <jorge.ortiz.fuentes@oracle.com>
2025-10-31 16:42:13 +0100

    Add constants and simplify tasks

--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -3,9 +3,30 @@
 // Automated build tasks for development and release to production.
 package main
 
-import "github.com/magefile/mage/sh"
+import (
+	"fmt"
+
+	"github.com/magefile/mage/sh"
+)
+
+const appName = "rexlists"
+const targetDir = "target"
 
 // Build front end application
-func Build() error {
-	return sh.Run("go", "build", "-o", "target/rexlists-fe", "cmd/rexlists-fe/main.go")
+func BuildFrontEnd() error {
+	fmt.Println("Building front end.")
+	return buildVariant(appName, "fe")
+
+}
+
+// Build back end application
+func BuildBackEnd() error {
+	fmt.Println("Building back end.")
+	return buildVariant(appName, "be")
+}
+
+func buildVariant(appName, variant string) error {
+	var bin = fmt.Sprintf("target/%s-%s", appName, variant)
+	var src = fmt.Sprintf("cmd/%s-%s/main.go", appName, variant)
+	return sh.Run("go", "build", "-o", bin, src)
 }
